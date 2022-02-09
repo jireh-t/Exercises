f8999bbbf18d0a5eaeedb4a43cb4128e4e9b6718
jireh-t <tsengj@middleton.school.nz>
2022-02-10 12:10:35 +1300

    Initial commit

---
+++ Exercise2.py
@@ -0,0 +1,6 @@
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+print("Hi {}, you are {}".format(name, age))
+
+#Tell user how old they will be in 5 years
+print("In 5 years time, you will be", age + 5)
---
+++ Intro Exercises.py
@@ -0,0 +1,15 @@
+print("Molly & Jireh are cool")
+print("hello jireh tseng")
+name=input("What is your name??")
+age=int(input("Enter your age"))
+print("Hello", name)
+food=input("What is your favourite food?")
+if food == "noodles":
+    print("yum! that is the best food")
+elif food == "potatos":
+    print("they are pretty nice but not as good as noodles")
+else:
+    print("NOOO NOODLES ARE THE BEST and kind of potatos")
+
+a=input("what is your name?")
+print(a.upper())
