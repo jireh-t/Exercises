84000ab08baf77db160f95e9caa06754be36764e
jireh-t <tsengj@middleton.school.nz>
2022-02-14 10:54:24 +1300

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -4,3 +4,17 @@ print("Hi {}, you are {}".format(name, age))
 
 #Tell user how old they will be in 5 years
 print("In 5 years time, you will be", age + 5)
+
+food= input("What is your favourite food?")
+print(f"Yummmm now I want {food}")
+
+print("Now for a quiz, ...")
+my_age=input("What is my age?")
+if my_age == "14":
+    print("You are correct!!")
+else:
+    print("NOPEEEEEEEEEE")
+
+print("That is the end of the quiz")
+a=input("Please enter your name one more time: ")
+print("Bye!", a.upper())
