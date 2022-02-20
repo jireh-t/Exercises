051e19beda6599236df6f6fe816841495614e7f9
jireh-t <tsengj@middleton.school.nz>
2022-02-21 11:25:51 +1300

    Update Exercise 9.py

--- Exercise 9.py
+++ Exercise 9.py
@@ -1,24 +1,7 @@
-#Number Printing Exercise
-
-
-for num in range (0, 1000001, 100):
-    print(num)
-
-#Input Number Exercise
 num = int(input("What number should I count to?"))
 for num in range (0, num + 1):
     print(num)
 
-#Random Exercise
-number = random.randint(1, 5)
-guess = int(input("What is the number???"))
-while guess != number:
-    if guess < number:
-        print("Guess Higher")
-        guess = int(input("What is the number???"))
-    else:
-        print("Guess Lower")
-        guess = int(input("What is the number???")
 
 
 
